08f339a98bb25a32f169a68564be98dfd6a76099
Eva Garcia Martin <eva.g.596@gmail.com>
2021-12-06 10:58:42 +0100

    Day 6: added tests

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ import day_2
 import day_3
 import day_4
 import day_5
+import day_6
 
 
 def test_day1(day, values):
@@ -65,15 +66,34 @@ def test_day5(day, values):
     print(f"All tests passed for {day}")
 
 
+def test_day6(day, values):
+    p1_res = values[0]
+    p2_res = values[1]
+
+    input = day_6.parse_input(day)
+
+    assert p1_res == day_6.count(input, 80), "Part 1 incorrect"
+    assert p2_res == day_6.count(input, 256), "Part 2 incorrect"
+    print(f"All tests passed for {day}")
+
+
 test_day1('day1_test', (7, 5))
 test_day1('day1', (1390, 1457))
+
 test_day2('day2_test', (150, 900))
 test_day2('day2', (2027977, 1903644897))
+
 test_day3('day3_test', (198, 230))
 test_day3('day3', (3687446, 4406844))
+
 test_day4('day4_test', (4512, 1924))
 test_day4('day4', (58838, 6256))
+
 test_day5('day5_test', (5, 12))
 test_day5('day5', (5576, 18144))
+
+test_day6('day6_test', (5934, 26984457539))
+test_day6('day6', (360268, 1632146183902))
+
 
 print("All tests passed")
