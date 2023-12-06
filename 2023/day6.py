4369880735214233c5460e00f34a0d18ee784781
Eva Garcia Martin <evag@spotify.com>
2023-12-06 10:03:57 +0100

    day 6 part 1

---
+++ 2023/day6.py
@@ -0,0 +1,17 @@
+def main():
+    part1_score = 1
+    with open("day6_input.txt") as f:
+        times = list(map(int, f.readline().strip().split(": ")[1].split()))
+        distances = list(map(int, f.readline().strip().split(": ")[1].split()))
+
+    for time, distance in zip(times, distances):
+        ways_to_win = sum(1 for i in range(time) if (time - i) * i > distance)
+        if ways_to_win > 0:
+            part1_score *= ways_to_win
+
+    print(f"Part 1: {part1_score}")
+    # print(f"Part 2: {}")
+
+
+if __name__ == "__main__":
+    main()
