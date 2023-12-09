01e2dabc35ea0f6602463dd5d3f2f5fdce856039
Eva Garcia Martin <evag@spotify.com>
2023-12-09 20:09:30 +0100

    solve day 9 part 2

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,17 +1,23 @@
+from functools import reduce
+
+
 def main():
-    extrapolated_sum = 0
+    part1_sum = 0
+    part2_sum = 0
     with open("day9_input.txt") as f:
         for line in f:
             history = list(map(int, line.strip().split()))
             last_history = [history[-1]]
+            first_history = [history[0]]
             while len(set(history)) > 1:
                 history = [y - x for x, y in zip(history[:-1], history[1:])]
                 last_history.append(history[-1])
+                first_history.append(history[0])
+            part1_sum += sum(last_history)
+            part2_sum += reduce(lambda x, y: y - x, reversed(first_history))
 
-            extrapolated_sum += sum(last_history)
-
-    print(f"Part 1: {extrapolated_sum}")
-    # print(f"Part 2: {}")
+    print(f"Part 1: {part1_sum}")
+    print(f"Part 2: {part2_sum}")
 
 
 if __name__ == "__main__":
