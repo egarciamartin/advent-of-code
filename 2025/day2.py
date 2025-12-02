a7c95246b40dff8039612c45409acb3719bedafe
Eva Garcia Martin <evag@spotify.com>
2025-12-02 12:39:38 +0100

    simplify the divisor part

--- 2025/day2.py
+++ 2025/day2.py
@@ -1,28 +1,19 @@
-
-def get_divisors(n) -> int:
-    divisors = []
-    for i in range(1, (n // 2) + 1):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
-
-def is_invalid_id(id_: str, part1=True) -> bool:
-    if part1:
-        if len(id_) % 2 != 0:
-            return False
-        divisions = [len(id_) // 2]
-    else:
-        if len(id_) % 2 != 0 and len(set(id_))== 1 and len(id_) > 1:
-            return True
-        divisions = get_divisors(len(id_))
+def is_invalid_id_p1(id_: str) -> bool:
+    return id_[:len(id_) // 2] == id_[len(id_) // 2:] 
     
-    for k in divisions:
-        divided_id = []
-        for i in range(0, len(id_), k):
-            divided_id.append(id_[i : i + k])
-        if len(set(divided_id)) == 1:
-            return True
 
+def is_invalid_id_p2(id_: str,) -> bool:
+    len_id = len(id_)
+    if len_id % 2 != 0 and len(set(id_))== 1 and len_id > 1:
+        return True
+    
+    for k in range(1, (len_id // 2) + 1):
+        if len_id % k == 0:
+            divided_id = []
+            for i in range(0, len_id, k):
+                divided_id.append(id_[i : i + k])
+            if len(set(divided_id)) == 1:
+                return True
     return False
 
 
@@ -37,9 +28,9 @@ def main():
     for pair in pairs:
         start, end = pair
         for i in range(start, end+1):
-            if is_invalid_id(str(i), part1=True):
+            if is_invalid_id_p1(str(i)):
                 total_part1 += i
-            if is_invalid_id(str(i), part1=False):
+            if is_invalid_id_p2(str(i)):
                 total_part2 += i
     
     print("Part 1", total_part1)
